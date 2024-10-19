ddbcbe7875fb37280768ccb28b925a58e56116f0
moemoeaye12345 <moeaye0095@gmail.com>
2024-10-19 22:59:31 +0630

    prepare database and table

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/register',function(){
+    return view('register');
 });
 
 Route::middleware([
